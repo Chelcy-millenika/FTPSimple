f53120fa61660e54180d6b9438bae961b1531b56
San Askaruly <s.askaruly@gmail.com>
2020-08-05 01:49:33 +0900

    Updating comments

--- app/src/main/java/com/soialab/askaruly/uploadftp2/MainActivity.java
+++ app/src/main/java/com/soialab/askaruly/uploadftp2/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.StrictMode;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,92 +17,92 @@ import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
-import com.jcraft.jsch.UIKeyboardInteractive;
-import com.jcraft.jsch.UserInfo;
 
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPSClient;
-
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    // UI elements
     Button connectServer;
     Button downloadServer;
     Button startCode;
 
-/*    String FTPSERVER = "mico.knu.ac.kr";
-    int PORT = 21;
-    String USERNAME = "sanzhar";
-    String PASSWORD = "unist";*/
+    // Credentials: define here
+    private String hostname = "something.unist.ac.kr"; // insert own hostname here
+    private String username = "someuser"; // insert own username here
+    private String password = "somepassword";
+    private int defaultPort = 22;
+    private String myBashCommand = "python simple.py";
 
-    //./adb push 'C:\Users\iptea\Pictures\download.jpg' '/data/'
+    // For upload: source is local & destination is sever
+    String sourceDirectory = "/storage/emulated/0/Android/data/com.example.android.myapp/files/somevideo.mp4"; // insert own source file path here
+    String destinationDirectory = "/home/someuser/somevideo.mp4"; // insert own destination directory here
 
-    String host = "mico.knu.ac.kr";
-    int port = 22;
-    int FTP_TIMEOUT = 100;
-    String user = "sanzhar";
-    String pass = "unist";
-    //String sourceDirectory = "/storage/emulated/0/Android/data/com.example.android.camera2video/files/1519781704861.mp4";
-    //String sourceDirectory = "/sdcard/57529.jpg";
-    //String sourceDirectory = "My files/Internal storage/Android/data/com.example.android.camera2video/files/1519781704861.mp4";
-    //String sourceDirectory = "/storage/Android/data/com.example.android.camera2video/files/1519781704861.mp4";
-    //String sourceDirectory = "/storage/Android/data/com.example.android.camera2video/files/1519781697754.mp4";
-    String sourceDirectory = "/storage/emulated/0/Android/data/com.example.android.camera2video/files/1519781704861.mp4";
-    String destinationDirectory = "/home/sanzhar/1519781704861.mp4";
+    // For download: source is server & destination is local
+    String remote_src_path = "/home/someuser/videoonserver.mp4"; // insert own remote source directory here
+    String lcl_src_path = "/storage/emulated/0/Android/data/com.example.android.myapp/files/videoonserver.mp4"; // insert own destination directory here
 
-    String remote_src_path = "/home/sanzhar/1519781704861.mp4";
-    String lcl_src_path = "/storage/emulated/0/Android/data/com.example.android.camera2video/files/jai.mp4";
 
-    //String destinationDirectory = "/home/sanzhar/57529.jpg";
+    // Permission variables
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Check permissions
         verifyStoragePermissions(this);
+
+        // Bind elements
         connectServer = (Button) findViewById(R.id.connectServer);
         downloadServer = (Button) findViewById(R.id.downloadServer);
         startCode = (Button) findViewById(R.id.startServerCode);
 
+        // Building policy
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+        // How to upload to server on buttonClick
         connectServer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 JSch jsch = new JSch();
                 Session session = null;
+
                 try {
                     Environment.getExternalStorageDirectory().getAbsolutePath();
 
-                    session = jsch.getSession("sanzhar", "mico.knu.ac.kr", 22); //default port is 22
+                    // Credentials
+                    session = jsch.getSession(username, hostname, defaultPort);
                     session.setConfig("StrictHostKeyChecking", "no");
-                    session.setPassword("unist".getBytes());
+                    session.setPassword(password.getBytes());
+
+                    // Start session and channel
                     session.connect();
                     Channel channel = session.openChannel("sftp");
                     channel.connect();
                     System.out.println("Connected");
 
+                    // Upload file
                     ChannelSftp sftp = (ChannelSftp) channel;
-
-                    //sftp.cd("home/sanzhar/");
                     sftp.put(sourceDirectory,destinationDirectory);
-                    Boolean success = true;
 
+                    Boolean success = true;
                     if(success){
-                        //
                         System.out.println("The file has been uploaded succesfully");
                     }
 
+                    // Close channel and session
                     channel.disconnect();
                     session.disconnect();
 
+                    // Exception handling
                 } catch (JSchException e) {
                     System.out.println(e.getMessage().toString());
                     e.printStackTrace(System.out);
@@ -111,12 +110,12 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println(e.getMessage().toString());
                     e.printStackTrace(System.out);
                 } finally {
-                    //session.disconnect();
                     System.out.println("Disconnected");
                 }
             }
         });
 
+        // How to download from server on buttonClick
         downloadServer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -125,28 +124,31 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     Environment.getExternalStorageDirectory().getAbsolutePath();
 
-                    session = jsch.getSession("sanzhar", "mico.knu.ac.kr", 22); //default port is 22
+                    // Credentials
+                    session = jsch.getSession(username, hostname, defaultPort);
                     session.setConfig("StrictHostKeyChecking", "no");
-                    session.setPassword("unist".getBytes());
+                    session.setPassword(password.getBytes());
+
+                    // Start session and channel
                     session.connect();
                     Channel channel = session.openChannel("sftp");
                     channel.connect();
                     System.out.println("Connected");
 
+                    // Download file
                     ChannelSftp sftp = (ChannelSftp) channel;
-
-                    //sftp.cd("home/sanzhar/");
                     sftp.get(remote_src_path,lcl_src_path);
-                    Boolean success = true;
 
+                    Boolean success = true;
                     if(success){
-                        //
                         System.out.println("The file has been downloaded succesfully");
                     }
 
+                    // Close channel and session
                     channel.disconnect();
                     session.disconnect();
 
+                    // Exception handling
                 } catch (JSchException e) {
                     System.out.println(e.getMessage().toString());
                     e.printStackTrace(System.out);
@@ -154,58 +156,47 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println(e.getMessage().toString());
                     e.printStackTrace(System.out);
                 } finally {
-                    //session.disconnect();
                     System.out.println("Disconnected");
                 }
             }
         });
 
-
+        // How to start custom script on server on buttonClick
         startCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 JSch jsch = new JSch();
                 Session session = null;
-                String command =("python simple.py");
 
                 try {
                     Environment.getExternalStorageDirectory().getAbsolutePath();
 
-                    session = jsch.getSession("sanzhar", "mico.knu.ac.kr", 22); //default port is 22
+                    // Credentials
+                    session = jsch.getSession(username, hostname, defaultPort);
                     session.setConfig("StrictHostKeyChecking", "no");
-                    session.setPassword("unist".getBytes());
+                    session.setPassword(password.getBytes());
+
+                    // Start session and channel
                     session.connect();
-                    //Channel channel = session.openChannel("sftp");
-                    //channel.connect();
-
-
-                    //ChannelSftp sftp = (ChannelSftp) channel;
-
-                    //SSH Channel
                     ChannelExec channelssh = (ChannelExec)session.openChannel("exec");
                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
                     channelssh.setOutputStream(baos);
 
-                    // Execute Command
-                    channelssh.setCommand("python simple.py");
+                    // Execute command
+                    channelssh.setCommand(myBashCommand);
                     channelssh.connect();
                     System.out.println("Connected");
 
-                    //sftp.cd("home/sanzhar/");
-                    //InputStream is = new ByteArrayInputStream(command.getBytes());
-                    //sftp.setInputStream(is);
-
                     Boolean success = true;
-
                     if(success){
-                        //
                         System.out.println("Command run successfully!");
                     }
 
+                    // Close channel and session
                     channelssh.disconnect();
-                    //channel.disconnect();
                     session.disconnect();
 
+                    // Exception handling
                 } catch (JSchException e) {
                     System.out.println(e.getMessage().toString());
                     e.printStackTrace(System.out);
@@ -213,20 +204,11 @@ public class MainActivity extends AppCompatActivity {
                     System.out.println(e.getMessage().toString());
                     e.printStackTrace(System.out);
                 } finally {
-                    //session.disconnect();
                     System.out.println("Disconnected");
                 }
             }
         });
-
-
     }
-
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
 
     public static void verifyStoragePermissions(Activity activity) {
         // Check if we have write permission
